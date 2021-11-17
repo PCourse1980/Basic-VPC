a92850242685c2bd374221a4bc0f6b4ac1ed4679
Julius Addi <oheeneeba@icloud.com>
2021-11-16 16:40:38 -0800

    basic_VPC

--- main.tf
+++ main.tf
@@ -69,3 +69,25 @@ resource "aws_route_table_association" "developer_private_route_association" {
     subnet_id = aws_subnet.developer_private_subnet.id
     route_table_id = aws_route_table.developer_private_route_table.id
 }
+
+
+#Internet Gateway
+
+resource "aws_internet_gateway" "devoloper_internet_Gateway" {
+  vpc_id = aws_vpc.developer_vpc.id
+
+  tags = {
+    name = "developer_internet_Gateway"
+  }
+  
+}
+
+# internet Gateway association with public route table
+
+resource "aws_route" "devloper_IGW_assoc_with_public_RT" {
+  route_table_id     = aws_route_table.developer_public_route_table.id
+  gateway_id         = aws_internet_gateway.devoloper_internet_Gateway.id
+  destination_cidr_block   = "0.0.0.0/0"
+  depends_on         = ["aws_route_table.developer_public_route_table"]
+
+}
--- securitygroup.tf
+++ securitygroup.tf
@@ -3,23 +3,23 @@
 resource "aws_security_group" "SG_HTTP" {
   name        = "allow_HTTP"
   description = "Allow TLS inbound traffic"
-  vpc_id      = aws_vpc.tut_proj6.id
+  vpc_id      = aws_vpc.developer_vpc.id
 
   ingress {
       description      = "HTTP from VPC"
-      from_port        = 443
-      to_port          = 443
+      from_port        = 80
+      to_port          = 80
       protocol         = "tcp"
-      cidr_blocks      = [aws_vpc.tut_proj6.cidr_block]
+      cidr_blocks      = [aws_vpc.developer_vpc.cidr_block]
       
     }
 
     ingress {
       description      = "SSH from VPC"
-      from_port        = 80
-      to_port          = 80
+      from_port        = 22
+      to_port          = 22
       protocol         = "tcp"
-      cidr_blocks      = [aws_vpc.tut_proj6.cidr_block]
+      cidr_blocks      = [aws_vpc.developer_vpc.cidr_block]
       
     }
 
